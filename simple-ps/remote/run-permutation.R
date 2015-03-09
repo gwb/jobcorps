046d95925561f19b0894272c9fe046f6b3a4771d
gwb <gw.basse@gmail.com>
2015-03-09 14:26:23 -0400

    adds script for running simul

---
+++ simple-ps/remote/run-permutation.R
@@ -0,0 +1,16 @@
+source("../generate.R", chdir=T)
+source("../estimation-automated.R", chdir=T)
+source("../inference_permutation.R", chdir=T)
+
+
+args <- commandArgs(trailingOnly = TRUE)
+tau <- as.numeric(args[2])
+
+
+#load("params.Rdata")
+load("theta.Rdata")
+
+
+res.permutation <- do.permutation.add.robust(W.y, y, X, post.strata.proba, CP, PS, S2O, MOD, tau=tau, maxiter=30,n.restart=5,eps=0.005)
+
+save(res.permutation, file=paste("/results/res.",args[1],".Rdata", sep=""))
