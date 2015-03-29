c66b8f416d1d8b43a8e81a3dea4c65e1990bc74b
gwb <gw.basse@gmail.com>
2015-03-29 10:26:17 -0400

    fixes run permutation

--- simple-ps/remote/run-permutation.R
+++ simple-ps/remote/run-permutation.R
@@ -10,6 +10,6 @@ load("simdata.Rdata")
 load("theta.Rdata")
 
 
-res.permutation <- do.permutation.add.robust(W.y, y, X, post.strata.proba, CP, PS, S2O, MOD, tau=tau, maxiter=30,n.restart=5,eps=0.005)
+res.permutation <- do.permutation.add.robust(W.y, y, X, post.strata.proba, CP, PS, S2O, MOD, tau=tau, maxiter=30,n.restart=6,eps=0.005)
 
 save(res.permutation, file=paste("results/res.",args[1],".Rdata", sep=""))
